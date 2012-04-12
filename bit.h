42f54a65087b5dfe5527fa02fbcc638eb82a4bfb
Matthew Temple <clownfysh@gmail.com>
2012-04-12 10:16:00 +0000

    tabified

--- bit.h
+++ bit.h
@@ -9,7 +9,7 @@ typedef enum ih_bit_t ih_bit_t;
 
 #define ih_bit_get(value, bit_index) ((value >> bit_index) & (uint64_t) 1)
 
-#define ih_bit_set(value, bit_index, bit_value)   \
+#define ih_bit_set(value, bit_index, bit_value)              \
   if (bit_value) { *value |= ((uint64_t) 1 << bit_index); }  \
   else { *value &= ~((uint64_t) 1 << bit_index); }
 
